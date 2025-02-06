c3ab96ba1a9ccd98e1c31edf52485e5f37ae2476
Kamil Yousuf <kamilyousuf0712@gmail.com>
2025-02-06 14:48:35 -0600

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Python Dev Environment with venv";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShell = pkgs.mkShell {
+        name = "python-dev";
+
+        buildInputs = [
+          pkgs.python3
+          pkgs.python3Packages.pip
+          pkgs.python3Packages.virtualenv
+        ];
+
+        nativeBuildInputs = [
+          pkgs.git
+          pkgs.direnv
+          pkgs.jq
+        ];
+
+        # Write custom config.fish to a temporary file and set XDG_CONFIG_HOME to point to it
+        shellHook = ''
+           export PS1="($name) $PS1"
+        '';
+      };
+    });
+}
