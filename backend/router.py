5ce9dc94056776f5e725b46172e15436927133de
Kamil Yousuf <kamilyousuf0712@gmail.com>
2025-02-11 17:05:29 -0600

    working on associating backend and database

--- backend/router.py
+++ backend/router.py
@@ -1,14 +1,20 @@
 from flask import Flask
 import firebase_admin
+import firebase.db_firestore
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return "Index Page"
 
 @app.route("/hello")
-def hello():
+def hello_world():
     return "Hello, World!"
+
+@app.route("/add_user/<username>")
+def add_user(username):
+    user = db_firestore.collection("users").document(f"{username}")
+    return "Success"
 
 
