c8a59ec8edf36c70830e98e3c9d22fe18972bf46
Kamil Yousuf <kamilyousuf0712@gmail.com>
2025-02-12 21:27:34 -0600

    backend can now receive json from frontend for user creation

--- backend/router.py
+++ backend/router.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import firebase_admin
 from firebase import db_firestore
 
@@ -12,15 +12,27 @@ def index():
 def hello_world():
     return "Hello, World!"
 
-@app.route("/add_user/<username>/<name>")
-def add_user(username, name):
+# TODO: make this more expansive; let's receive JSON from the frontend
+# this will give us more flexibility
+@app.route("/add_user", methods=['POST'])
+def add_user(username):
+    data = request.get_json()
+
+    # Extract fields; what fields?
+    # I think a (non-personal) location of origin would actually be really cool; let's add that!
+    username = data.get("username")
+    name = data.get("name")
+    location = data.get("location")
+
     user = db_firestore.collection("users").document(f"{username}")
-    user.set({"name": f"{name}"})
-    return "Success"
+    user.set({"name": f"{name}",
+              "location": f"{location}"})
+
+    return jsonify({"message" :f"Received name: {name}, location: {location}"}), 200
 
 @app.route("/get_user/<username>")
 def get_user(username):
-    user = db_firestore.collection("users").document(f"{username}")
-    return user
+    user = db_firestore.collection("users").document(f"{username}").get()
+    return user.to_dict()
 
 
